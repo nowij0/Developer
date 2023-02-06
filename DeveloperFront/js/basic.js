1ec3fa24a36a3e9367068d759fd528dd0a6dc33c
nowij0 <moonone470@gmail.com>
2023-02-06 23:46:20 +0900

    Revert "수업 프론트단 작성 중"
    This reverts commit 34ca40325ef305a12e04b71a64ca5fd843190572.

--- DeveloperFront/js/basic.js
+++ DeveloperFront/js/basic.js
@@ -1,5 +1,7 @@
-let backURL = "http://192.168.0.28:8888/developer/";
-let frontURL = "http://192.168.0.28:5500/html/";
+let backURL = "http://localhost:8888/developer/";
+let frontURL = "http://localhost:5500/html/";
+
+//바꿔야함 맞춰서 다시
 
 //--[회원] 로그인상태의 메뉴들 보여주기 함수 START--
     function showMenuAtMemberLogined() {
@@ -75,7 +77,7 @@ function checkLogined() {
         let menu = $(e.target).attr('class')
         switch (menu) {
             case 'lesson':
-                location.href = './lesson.html'
+                $('section').load('./lesson.html')
                 break;
             case 'studycafe':
                 location.href= backURL + 'studycafe.html';
