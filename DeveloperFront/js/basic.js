dde76015fe35488559e722a9519d1e5f1d2ce8db
nowij0 <moonone470@gmail.com>
2023-02-14 00:10:04 +0900

    index파일 삭제 및 담당 구현 파트 css/js/html 작업

--- DeveloperFront/js/basic.js
+++ DeveloperFront/js/basic.js
@@ -1,5 +1,5 @@
-// let backURL = "http://192.168.0.198:8888/developer/";
-// let frontURL = "http://192.168.0.198:5500/html/";
+// let backURL = "http://192.168.0.62:8888/developer/";
+// let frontURL = "http://192.168.0.62:5500/html/";
 
 let backURL = "http://192.168.0.28:8888/developer/";
 let frontURL = "http://192.168.0.28:5500/html/";
--- DeveloperFront/js/lesson/lessonlist.js
+++ DeveloperFront/js/lesson/lessonlist.js
@@ -1,92 +1,69 @@
-// $(() => { 
+//=== 수업 목록 보여주기 START ===
+$(()=>{
+    let url = backURL + 'lesson/lessonlist';
+    let data = "category=1&priceFilter=1&starFilter=2";
 
-//     function showList(url, page){
-//         let $origin = $('div.lesson').first();
-//         $('div.lesson').not(':first-child').remove(); 
-//         $origin.show(); 
+    $.ajax({
+        url : url,
+        data: data,
+        success: function(jsonObj){
+            console.log(jsonObj)
 
-//         $.ajax({
-//             url : url,
-//             method: 'get',
-//             data: 'currentPage=' + page,
-//             success: function(jsonObj){ 
-//                 let list = jsonObj.list;
-//                 let totalPage = jsonObj.totalPage;
-//                 let currentPage = jsonObj.currentPage;
-//                 let startPage = jsonObj.startPage;
-//                 let endPage = jsonObj.endPage;
+            let $origin = $('#lesson').first();
+            let $parent = $('#lessonList');
+            $(jsonObj).each((index)=>{
+                let category = jsonObj[index].category;
+                if(category == 0){
+                    category = '프로그래밍 언어';
+                } else if(category == 1){
+                    category = '웹 개발';
+                } else if(category == 2){
+                    category = '앱 개발';
+                } else if(category == 3){
+                    category = '보안/네트워크';
+                } else if(category == 4){
+                    category = '데이터';
+                }
+                
+                let $copy = $origin.clone();
+                $copy.find('div.lCategory').html(category)
+                let $imgObj = $('<img>')
+                $imgObj.attr('class', 'lessonImg');
+                $imgObj.attr('src', '/images/' + jsonObj[index].imgPath);
+                $copy.find('div.img').empty().append($imgObj);
+                $copy.find('div.lessonName').html("<h6>" + jsonObj[index].lessonName + "</h6>")
+                $copy.find('div.tutorName').html(jsonObj[index].tutorVO.usersVO.name)
+                
+                let price = jsonObj[index].price;
+                if(price == 0){
+                    price = '무료'
+                }
+                $copy.find('div.price').html(price)
+                $copy.find('div.cDate').html(jsonObj[index].startCdate + " ~ " + jsonObj[index].endCdate)
 
-//                 //원본
-//                 let $origin = $('div.lesson').first();  //$가 붙어있으면 jQuery용 변수
-//                 let $parent = $('div.lessonList');
-//                 $(list).each((index, p) => {
-//                     let prodNo = p.prodNo;
-//                     let prodName = p.prodName;
-//                     let prodPrice = p.prodPrice;
+                $parent.append($copy);
+            });
+            $origin.hide();
+        }, 
+        error: function(xhr){
+            let jsonObj = JSON.parse(xhr.responseText);
+            alert(jsonObj.msg);
+        }
+    });
+});
+//=== 수업 목록 보여주기 END ===
 
-//                     let $copy = $origin.clone();
-//                     let $imgObj = $('<img>') //img태그 객체를 만드는 것
-//                     $imgObj.attr('src', '../images/' + prodNo + '.jpg');
-//                     $copy.find('div.img').empty().append($imgObj);
-//                     $copy.find('div.prodNo').html(prodNo); // .html() : div태그의 내부의 영역
-//                     $copy.find('div.prodName').html(prodName);
-//                     $copy.find('div.prodPrice').html(prodPrice + '원') ;
-
-//                     $parent.append($copy); //부모의 마지막 요소로 추가
-//                 });
-//                 $origin.hide(); //원본 껍데기 숨기기
-
-//                 let $pageGroup = $('div.pagegroup');
-//                 let pageGroupStr = "";
-
-//                 //무조건 startPage의 이전 페이지가 있다
-//                 if(startPage > 1){ 
-//                     pageGroupStr += '<span class="' + (startPage-1) + '">[PREV}</span>'; //<span>은 대표적인 인라인태그
-//                 }
-
-//                 //총합을 넘어서지 못하게 totalPage값을 대입 시키기
-//                 if(endPage > totalPage){ 
-//                     endPage = totalPage;
-//                 }
-
-//                 //for문 돌려서 페이지 넘버링
-//                 for(let i=startPage; i<=endPage; i++){
-//                     if(i == currentPage){ //현재페이지 구분시키기
-//                         pageGroupStr += '<span class="current' + i + '">[' + i + ']</span>'; 
-//                     } else {
-//                         pageGroupStr += ' <span class="' + i + '">[' + i + ']</span>';
-//                     }
-//                 }
-//                 //무조건 endPage의 다음 페이지가 있다. 
-//                 if(endPage < totalPage){
-//                     pageGroupStr += '<span class="' + (endPage+1) + '">[NEXT]</span>';
-//                 }
-//                 $pageGroup.html(pageGroupStr);
-//             },
-//             error:function(xhr){
-//                 let jsonObj =  JSON.parse(xhr.responseText);
-//                 alert(jsonObj.msg);
-//             }
-//         });
-//     };
-//     let url = backURL + '/product/list';
-
-//     // == 상품목록 요청 START
-//     //무조건 첫페이지부터 출력시켜주기
-//     showList(url, 1); 
-//     // == 상품목록 요청 END
-
-//     // == 페이지번호 클릭 START
-//     $('div.pagegroup').on('click', 'span:not(.current)', (e) => { //span 태그가 current가 아닌 애들을 클릭했을 때
-//         let page = $(e.target).attr('class');
-//         showList(url, page);
-//     });
-//     // == 페이지번호 클릭 END
-
-//     // == 상품 상세정보 START
-//     $('div.productlist').on('click', 'div.product', (e) => {
-//         let prodNo = $(e.target).parents('div.product').find('div.prodNo').html();
-//         location.href = './productinfo.html?prodNo=' + prodNo;
-//     });
-//     // == 상품 상세정보 END
-// });
+//=== 로고/수업 이미지/이름/강사명 누르면 해당 페이지로 이동 START ===
+$(document).on('click', '#logo', function(){
+    location.href = frontURL + 'index.html';
+});
+$(document).on('click', '#lesson > div.img', function(){
+    location.href= frontURL + 'lesson/detail.html';
+});
+$(document).on('click', '#lesson > div.lessonName', function(){
+    location.href= frontURL + 'lesson/detail.html';
+});
+$(document).on('click', '#lesson > div.tutorName', function(){
+    location.href= frontURL + 'lesson/tutorinfo.html';
+});
+//=== 로고/수업 이미지/이름/강사명 누르면 해당 페이지로 이동 END ===
